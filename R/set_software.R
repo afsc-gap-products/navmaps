d1df17d4b40011c34aec127e621d8ced3c7f1603
sean-rohan-NOAA <sean.rohan@noaa.gov>
2023-01-06 09:14:20 -0800

    Function to set file type extensions and software name in the global environment to streamline switching software

---
+++ R/set_software.R
@@ -0,0 +1,32 @@
+#' Set global variables for file extensions and software type
+#' 
+#' @param x software name as a character vector. Options: globe, opencpn, timezero
+#' @export
+
+set_software <- function(x) {
+  
+  .check_software(software_format = x)
+  
+  message("Assigning FILE_TYPE_POLYGON, FILE_TYPE_LINESTRING, FILE_TYPE_POINT, and SOFTWARE to the the global environment.")
+  
+  assign("SOFTWARE", value = x, envir = .GlobalEnv)
+  
+  if(x == "timezero") {
+    assign("FILE_TYPE_POLYGON", value = "kml", envir = .GlobalEnv)
+    assign("FILE_TYPE_LINESTRING", value = "kml", envir = .GlobalEnv)
+    assign("FILE_TYPE_POINT", value = "gpx", envir = .GlobalEnv)
+  }
+  
+  if(x == "opencpn") {
+    assign("FILE_TYPE_POLYGON", value = "gpx", envir = .GlobalEnv)
+    assign("FILE_TYPE_LINESTRING", value = "gpx", envir = .GlobalEnv)
+    assign("FILE_TYPE_POINT", value = "gpx", envir = .GlobalEnv)
+  }
+  
+  if(x == "globe") {
+    assign("FILE_TYPE_POLYGON", value = "mdb", envir = .GlobalEnv)
+    assign("FILE_TYPE_LINESTRING", value = "mdb", envir = .GlobalEnv)
+    assign("FILE_TYPE_POINT", value = "mdb", envir = .GlobalEnv)
+  }
+  
+}
