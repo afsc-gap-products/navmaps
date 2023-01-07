5900348a084e0dc02c086ca8ad474e5f77130ec3
sean-rohan-NOAA <sean.rohan@noaa.gov>
2023-01-06 16:38:23 -0800

    Function to import Globe data from .mdb and convert to simple features points or linestrings

---
+++ R/globe_to_sf.R
@@ -0,0 +1,89 @@
+#' Read Globe data from .mdb or .accdb and convert to simple features
+#' 
+#' Read Globe marks or lines from a Microsoft Access Database (.mdb or .accdb) and convert to an sf POINT or LINESTRING object.
+#' 
+#' @param dsn Path to a Globe .mdb or .accdb file.
+#' @param tablename Optional. tablename as a chracter vector (typically "marks" or "lines"). The function attempts to detect the layer type if not provided.
+#' @param wkt_geometry_type Optional. Geometry type for output as a character vector; must be either POINT or LINESTRING. Typically "POINT" for marks or "LINESTRING" for lines.
+#' @param grouping_col Grouping column for LINESTRINGS.
+#' @return A simple features object
+#' @export
+
+globe_to_sf <- function(dsn, tablename = NULL, wkt_geometry_type = NULL, grouping_col = NULL) {
+  
+  .check_driver()
+  
+  if(!file.exists(dsn)) {
+    stop("read_globe: ", dsn, " not found.")
+  }
+  
+  message("write_to_access: Connecting to ", dsn)
+  odbc_con <- RODBC::odbcDriverConnect(paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", dsn))
+  
+  if(is.null(tablename)) {
+    tablename <- c("lines", "marks")[which(c("lines", "marks") %in% RODBC::sqlTables(channel = odbc_con)$TABLE_NAME)]
+    
+    stopifnot("read_globe: tablename argument was not specified and the Access database did not contain EXACTLY one table named lines or marks. Please provide the tablename argument in the read_globe() function call." = length(tablename) == 1)
+  }
+  
+  message("read_globe: Reading data from ", dsn)
+  dat <- RODBC::sqlQuery(channel = odbc_con, query = paste0("select * from ", tablename))
+  
+  message("read_globe: Imported ", nrow(dat), " lines from ", dsn)
+  
+  # When wkt_geometry_type is not provided, attempt to detect object type (LINESTRING or POINTS) automatically by checking for blank lines between coordinates
+  if(is.null(wkt_geometry_type)) {
+    
+    diff_lat <- is.na(diff(dat$Latitude))
+    
+    if(sum(diff_lat[1:(length(diff_lat)-1)]) > 0) {
+      wkt_geometry_type <- "LINESTRING"
+    } else {
+      wkt_geometry_type <- "POINT"
+    }
+    
+  } else {
+    wkt_geometry_type <- toupper(wkt_geometry_type)
+    
+    stopifnot("read_globe: wkt_geometry_type must be LINESTRING or POINT." = wkt_geometry_type %in% c("LINESTRING", "POINT"))
+    
+  }
+  
+  # Convert latitude and longtiude to decimal degrees 
+  dat$Latitude <- radians_to_dd(dat$Latitude)
+  dat$Longitude <- radians_to_dd(dat$Longitude)
+  
+  if(any(c("Index", "index", "INDEX") %in% names(dat))) {
+    dat <- dat[, -which(names(dat) %in% c("Index", "index", "INDEX"))]
+  }
+  
+  if(is.null(grouping_col)) {
+    message("read_globe: Setting grouping column")
+    dat$temp_grp_col <- cumsum(is.na(dat$Latitude))
+    grouping_col <- "temp_grp_col"
+  }
+  
+  message("read_globe: Making sf POINT object")
+  out <- dat |>
+    dplyr::filter(!is.na(Latitude), !is.na(Longitude)) |>
+    sf::st_as_sf(coords = c("Longitude", "Latitude"), 
+                 crs = "EPSG:4326")
+  
+  if(wkt_geometry_type == "LINESTRING") {
+    
+    message("read_globe: Converting to LINESTRING")
+    out <- dplyr::rename(out, id = grouping_col) |>
+      dplyr::select(id, geometry)
+    
+    out <- out |>
+      dplyr::group_by(id) |>
+      dplyr::summarise() |>
+      sf::st_cast(to = "LINESTRING") |>
+      sf::st_wrap_dateline() |>
+      dplyr::mutate(id = as.character(id))
+    
+  }
+  
+  return(out)
+  
+}
