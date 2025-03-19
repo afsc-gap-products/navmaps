68d831fb725b812e35234d1fe1b3102b9daa85c0
Sean Rohan <sean.rohan@noaa.gov>
2025-03-19 14:50:04 -0700

    Add ZO's TSP path

--- R/planning_solve_station_tsp.R
+++ R/planning_solve_station_tsp.R
@@ -1,18 +1,18 @@
-#' Solve a traveling Salesman problem (TSP) for a survey station alaocation
+#' Solve a traveling Salesman problem (TSP) for a survey station allocation
 #'
-#' This function optimizes the sampling order of stations by solving a Traveling Salesman Problem (TSP) using the nearest insertion method to minimize the distance traveled.
+#' This function optimizes the sampling order of stations by solving a Traveling Salesman Problem (TSP) using the nearest insertion method to minimize the distance traveled. Option to use a circuit (starting and ending at the same station) or path (visit each station once without repeats).
 #'
 #' @param x An `sf` object representing the survey stations.
+#' @param hamilton Should a Hamiltonian path be calculated instead of a circuit?
 #'
 #' @return A list containing:
 #' \item{distance_nodes}{An `sf` object of survey stations ordered based on 
 #' the optimal TSP solution, with an added column for inter-station distances in kilometers.}
 #' \item{tsp}{The TSP solution object.}
-#'
 #' @importFrom sf st_distance st_transform
 #' @importFrom TSP as.ATSP TSP solve_TSP
 #' @importFrom dplyr filter
-#'
+#' @author Zack Oyafuso
 #' @examples
 #' \dontrun{
 #' # Load and transform survey stations
@@ -39,36 +39,45 @@
 #' }
 #'
 #' @export
-#' 
+
 planning_solve_station_tsp <- 
-  function(x) {
+  function(x, hamilton = TRUE) {
     
-    vessel_tsp <- x |>
+    tsp_data <- x |>
       sf::st_distance() |>
       as.matrix() |> 
       matrix(
         nrow = nrow(x), 
         ncol = nrow(x)
       ) |>
-      TSP::as.ATSP() |> 
-      TSP::TSP() |> 
-      TSP::solve_TSP(method = "nearest_insertion")
+      TSP::TSP()
     
-    x$node <- as.numeric(attr(vessel_tsp, "names"))
-    x <- x[as.numeric(attr(vessel_tsp, "names")), ]
+    if (hamilton) {
+      # Insert dummy city to hack the solver to calculate the hamiltonian path
+      tsp_data <- TSP::insert_dummy(tsp_data, label = "cut")
+    }
+    
+    tsp_solution <- TSP::solve_TSP(x = tsp_data,
+                                   method = "nearest_insertion")
+    
+    if (hamilton) {
+      # Insert dummy city to hack the solver to calculate the hamiltonian path
+      tsp_solution <- TSP::cut_tour(tsp_solution, "cut")
+    }
+    
+    x$node <- as.numeric(attr(tsp_solution, "names"))
+    x <- x[as.numeric(attr(tsp_solution, "names")), ]
     x$order <- 1:nrow(x)
     x$distance <- c(0,
-                              sf::st_distance(x = x[1:(nrow(x)-1), ], 
-                                              y = x[2:nrow(x), ], 
-                                              by_element = TRUE)
+                    sf::st_distance(x = x[1:(nrow(x)-1), ], 
+                                    y = x[2:nrow(x), ], 
+                                    by_element = TRUE)
     )
     
     x$distance <- as.numeric(x$distance / 1000)
     
-    return(
-      list(
-        distance_nodes = x, 
-        tsp = vessel_tsp)
+    return(list(distance_nodes = x, 
+                tsp = tsp_solution)
     )
     
   }
